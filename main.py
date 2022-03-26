6b6a965645bbff4b6e880cbb0c8dc814791e98b9
h-dew <benjaminscown@gmail.com>
2022-03-26 12:11:45 -0700

    added setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import board
+
+from kmk.matrix import DiodeOrintation
+
+col_pins = (board.GP0, board.GP1, board.GP2, board.GP4, board.GP5, board.GP6)
+row_pins = (board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12)
+
+
+
+if __name__ == '__main__':
+    keyboard.go()
